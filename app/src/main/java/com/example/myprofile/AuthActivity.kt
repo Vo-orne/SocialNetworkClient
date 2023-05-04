1fdec16b59304c9bac4d4c65a7c35823e4ecdd23
Vo-orne <tolya.rudyak2012@gmail.com>
2023-05-04 15:51:07 +0300

    SNC-1: change from 04.05.23 Changed the input text color in TextInputLayout to match the design. Changed the start activity to AuthActivity. Added launch of MainActivity by clicking on the registration button.

--- app/src/main/java/com/example/myprofile/AuthActivity.kt
+++ app/src/main/java/com/example/myprofile/AuthActivity.kt
@@ -1,11 +1,44 @@
 package com.example.myprofile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.textfield.TextInputLayout
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val registerButton: AppCompatButton = findViewById(R.id.buttonRegister)
+//        val receivedUserEmail: TextInputLayout = findViewById(R.id.editTextEmailAddress)
+//
+//        val userName: String = parsEmail(receivedUserEmail)
+
+        registerButton.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+//                it.putExtra("isItFromStart", true)
+//                it.putExtra("userName", userName)
+                startActivity(it)
+                finish()
+            }
+        }
+    }
+
+    private fun parsEmail(receivedUserEmail: TextInputLayout): String {
+        val source: String = receivedUserEmail.toString()
+        var result = ""
+
+        for(i in source.indices) {
+            if (source[i] == '@') {
+                return result
+            } else if (source[i] == '.') {
+                result += " "
+                continue
+            }
+            result += source[i]
+        }
+        return ""
     }
 }
--- app/src/main/java/com/example/myprofile/MainActivity.kt
+++ app/src/main/java/com/example/myprofile/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myprofile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val registerButton: AppCompatButton = findViewById(R.id.viewMyContacts)
+
+//        val isItFromStart = intent.getBooleanExtra("isItFromStart", false)
+//        val newUserName = intent.getStringExtra("userName")
+//
+//        if(isItFromStart) {
+//            findViewById<TextView>(R.id.userName).apply {
+//                text = newUserName
+//            }
+//        }
+
+        registerButton.setOnClickListener {
+            Intent(this, AuthActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
