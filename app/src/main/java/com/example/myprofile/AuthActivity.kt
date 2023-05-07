c10a1781ca646c7d9aa2c2645738ef5bed7838ef
Vo-orne <tolya.rudyak2012@gmail.com>
2023-05-07 21:14:05 +0300

    SNC-1: change from 07.05.23 - Added screen transition animation. - Upload your own photo on the main screen) - Saving login and password data in the phone's local storage has been added, but requires refinement.

--- app/src/main/java/com/example/myprofile/AuthActivity.kt
+++ app/src/main/java/com/example/myprofile/AuthActivity.kt
@@ -1,35 +1,74 @@
 package com.example.myprofile
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.CheckBox
 import androidx.appcompat.widget.AppCompatButton
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
         val registerButton: AppCompatButton = findViewById(R.id.buttonRegister)
-        val receivedUserEmail: TextInputLayout = findViewById(R.id.editTextEmailAddress)
-        val x = findViewById<TextInputEditText>(R.id.textInputEditTextAddress)
+
+        sharedPreferences = getSharedPreferences("login_data", Context.MODE_PRIVATE)
+        val receivedUserEmail = findViewById<TextInputEditText>(R.id.textInputEditTextEmail)
+        val receivedUserPassword = findViewById<TextInputEditText>(R.id.textInputEditTextPassword)
+
+        val email = getLoginData().first
+        val password = getLoginData().second
+
+        receivedUserEmail.setText(email)
+        receivedUserPassword.setText(password)
+
+        val memberInputDate = findViewById<CheckBox>(R.id.memberInputDate)
 
         registerButton.setOnClickListener {
             Intent(this, MainActivity::class.java).also {
-                val userName: String = parsEmail(receivedUserEmail, x)
+                val newEmail: String = receivedUserEmail.text.toString()
+                val newPassword = receivedUserPassword.text.toString()
+                saveLoginData(newEmail, newPassword)
+
+                val userName: String = parsEmail(receivedUserEmail)
                 it.putExtra("userName", userName)
                 startActivity(it)
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
                 finish()
             }
         }
     }
 
-    private fun parsEmail(receivedUserEmail: TextInputLayout, x: TextInputEditText): String {
-        val source: String = x.text.toString()
+    private fun getLoginData(): Pair<String?, String?> {
+        val username = sharedPreferences.getString("username", null)
+        val password = sharedPreferences.getString("password", null)
+        return Pair(username, password)
+    }
+
+    private fun saveLoginData(username: String, password: String) {
+        with(sharedPreferences.edit()) {
+            putString("username", username)
+            putString("password", password)
+            apply()
+        }
+    }
+
+    fun saveAutoLoginState(autoLogin: Boolean) {
+        with(sharedPreferences.edit()) {
+            putBoolean("autoLogin", autoLogin)
+            apply()
+        }
+    }
+
+    private fun parsEmail(receivedUserEmail: TextInputEditText): String {
+        val source: String = receivedUserEmail.text.toString()
         var result = ""
 
         for (i in source.indices) {
