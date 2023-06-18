47b7fe604b0532e2f5848a25ce7cd99bc0381547
Vo-orne <tolya.rudyak2012@gmail.com>
2023-06-18 23:13:34 +0300

    SNC-2: change from 18.06.23 - Developed a custom class that is responsible for the Google registration button so that it fully matches the design.

--- app/src/main/java/com/example/myprofile/auth/GoogleButton.kt
+++ app/src/main/java/com/example/myprofile/auth/GoogleButton.kt
@@ -1,16 +1,14 @@
 package com.example.myprofile.auth
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
+import android.content.res.TypedArray
+import android.graphics.Color
 import android.graphics.drawable.Drawable
-import android.text.TextUtils
+import android.graphics.drawable.InsetDrawable
 import android.util.AttributeSet
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.content.res.ResourcesCompat
 import com.example.myprofile.R
 
 class GoogleButton @JvmOverloads constructor(
@@ -18,119 +16,81 @@ class GoogleButton @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = android.R.attr.buttonStyle
 ) : AppCompatButton(context, attrs, defStyleAttr) {
-    private var iconGravity: Int = 0
-    private var iconSize: Int = 0
-    private var icon: Drawable? = null
+
+    private val defaultTextSize: Int = 15
+    private val defaultTextColor = Color.BLACK
+    private var iconPadding: Int = 0
 
     init {
-        val typedArray = context.obtainStyledAttributes(
-            attrs, R.styleable.GoogleButton, defStyleAttr, 0
-        )
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.GoogleButton)
 
-        val buttonColor = typedArray.getColor(
-            R.styleable.GoogleButton_buttonColor, ContextCompat.getColor(context, R.color.white)
-        )
-        icon = typedArray.getDrawable(
-            R.styleable.GoogleButton_icon
-        )
-        val text = typedArray.getString(
-            R.styleable.GoogleButton_text
-        )
-        val textSize = typedArray.getDimensionPixelSize(
-            R.styleable.GoogleButton_android_textSize, 0
-        ).toFloat()
-        val textAllCaps = typedArray.getBoolean(
-            R.styleable.GoogleButton_android_textAllCaps, false
-        )
-        val textColor = typedArray.getColor(
-            R.styleable.GoogleButton_android_textColor, ContextCompat.getColor(context, R.color.black)
-        )
-        val buttonBackground = typedArray.getResourceId(
-            R.styleable.GoogleButton_buttonBackground, R.drawable.frame_button_google
-        )
-        iconGravity = typedArray.getInt(
-            R.styleable.GoogleButton_iconGravityX, 0
-        )
-        iconSize = typedArray.getDimensionPixelSize(
-            R.styleable.GoogleButton_iconSizeX, 0
-        )
+        setButtonText(typedArray)
+        setButtonTextSize(typedArray)
+        setButtonTextColor(typedArray)
+        setButtonTextFontFamily(typedArray)
+        setButtonBackground(typedArray)
+        setButtonIcon(typedArray)
+        setButtonIconPadding(typedArray)
 
         typedArray.recycle()
-
-        setBackgroundColor(buttonColor)
-        setBackgroundResource(buttonBackground)
-        this.text = text
-        setTextSize(textSize)
-        isAllCaps = textAllCaps
-        setTextColor(textColor)
-
-        updateIconGravity(icon, text)
     }
 
-    private fun updateIconGravity(icon: Drawable?, text: CharSequence?) {
-        val compoundDrawables = compoundDrawablesRelative
-        val iconDrawable = icon?.let { DrawableCompat.wrap(it).mutate() }
-
-        val textBounds = Rect()
-        paint.getTextBounds(text.toString(), 0, text.toString().length, textBounds)
-        val textWidth = textBounds.width()
-
-        when (iconGravity) {
-            GRAVITY_START -> setCompoundDrawablesRelativeWithIntrinsicBounds(
-                iconDrawable, null, null, null
-            )
-            GRAVITY_END -> setCompoundDrawablesRelativeWithIntrinsicBounds(
-                null, null, iconDrawable, null
-            )
-            GRAVITY_TEXT_START -> {
-                setCompoundDrawablesRelativeWithIntrinsicBounds(
-                    iconDrawable, null, null, null
-                )
-                setPadding(iconSize, 0, 0, 0)
-            }
-            GRAVITY_TEXT_END -> {
-                setCompoundDrawablesRelativeWithIntrinsicBounds(
-                    null, null, iconDrawable, null
-                )
-                setPadding(0, 0, iconSize, 0)
-            }
-        }
-        compoundDrawablePadding = iconSize + (textWidth / 2)
+    private fun setButtonText(typedArray: TypedArray) {
+        text = typedArray.getString(R.styleable.GoogleButton_text)
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val width = measuredWidth + compoundDrawablePadding + iconSize
-        setMeasuredDimension(width, measuredHeight)
+    private fun setButtonTextSize(typedArray: TypedArray) {
+        val customTextSize = typedArray.getDimensionPixelSize(
+            R.styleable.GoogleButton_android_textSize,
+            defaultTextSize
+        )
+        textSize = (customTextSize / 2).toFloat()
     }
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        if (!TextUtils.isEmpty(text)) {
-            val textBounds = Rect()
-            paint.getTextBounds(text.toString(), 0, text.toString().length, textBounds)
-            val textWidth = textBounds.width()
+    private fun setButtonTextColor(typedArray: TypedArray) {
+        val customTextColor = typedArray.getColor(
+            R.styleable.GoogleButton_android_textColor,
+            defaultTextColor
+        )
+        setTextColor(customTextColor)
+    }
 
-            val compoundDrawables = compoundDrawablesRelative
-            val iconDrawable = icon?.let { DrawableCompat.wrap(it).mutate() }
-            val iconPadding = compoundDrawablePadding
-
-            val totalDrawableWidth = if (iconDrawable != null) {
-                iconDrawable.intrinsicWidth + iconPadding
-            } else {
-                0
-            }
-
-            val textX = (width - textWidth - totalDrawableWidth) / 2
-            val textY = (height + textBounds.height()) / 2
-            canvas.drawText(text.toString(), textX.toFloat(), textY.toFloat(), paint)
+    private fun setButtonTextFontFamily(typedArray: TypedArray) {
+        val fontFamily = typedArray.getResourceId(R.styleable.GoogleButton_textFontFamily, 0)
+        if (fontFamily > 0) {
+            typeface = ResourcesCompat.getFont(context, fontFamily)
         }
     }
 
-    companion object {
-        private const val GRAVITY_START = 0
-        private const val GRAVITY_END = 1
-        private const val GRAVITY_TEXT_START = 2
-        private const val GRAVITY_TEXT_END = 3
+    private fun setButtonBackground(typedArray: TypedArray) {
+        val customBackgroundResId = typedArray.getResourceId(
+            R.styleable.GoogleButton_buttonBackground, R.drawable.frame_button_google
+        )
+        val customBackground: Drawable? = ContextCompat.getDrawable(context, customBackgroundResId)
+        background = customBackground
+    }
+
+    private fun setButtonIcon(typedArray: TypedArray) {
+        val customImageResId = typedArray.getResourceId(
+            R.styleable.GoogleButton_icon,
+            R.drawable.frame_button_google
+        )
+        val customImage = ContextCompat.getDrawable(context, customImageResId)
+        iconPadding =
+            typedArray.getDimensionPixelSize(R.styleable.GoogleButton_iconPaddingLeft, 0)
+        val insetDrawable = InsetDrawable(customImage, iconPadding, 0, 0, 0)
+
+        setCompoundDrawablesRelativeWithIntrinsicBounds(insetDrawable, null, null, null)
+    }
+
+    private fun setButtonIconPadding(typedArray: TypedArray) {
+        // Getting the distance between the button's icon and text
+        val iconTextSpacing = typedArray.getDimensionPixelSize(
+            R.styleable.GoogleButton_iconTextSpacing,
+            0
+        )
+
+        // Setting the distance between the button icon and text
+        compoundDrawablePadding = iconPadding + iconTextSpacing
     }
 }
