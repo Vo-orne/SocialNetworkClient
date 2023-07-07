1669d1d6ab282ffa540d9e7495a4eb88f079ce37
Vo-orne <tolya.rudyak2012@gmail.com>
2023-07-07 18:38:39 +0300

    SNC-3: change from 07.07.23 - Added swipeToDelete;

--- app/src/main/java/com/example/myprofile/fragments/MyContactsFragment.kt
+++ app/src/main/java/com/example/myprofile/fragments/MyContactsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myprofile.fragments
 
+import androidx.recyclerview.widget.ItemTouchHelper
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myprofile.R
 import com.example.myprofile.databinding.FragmentMyContactsBinding
 import com.example.myprofile.mycontacts.*
@@ -27,7 +29,7 @@ class MyContactsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMyContactsBinding.inflate(inflater, container, false)
-        adapter = ContactsAdapter(object: ContactActionListener {
+        adapter = ContactsAdapter(object : ContactActionListener {
 
             override fun onContactDelete(contact: Contact) {
                 viewModel.deleteUser(contact)
@@ -45,6 +47,7 @@ class MyContactsFragment : Fragment() {
         val layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerViewContacts.layoutManager = layoutManager
         binding.recyclerViewContacts.adapter = adapter
+        swipeToDelete()
         return binding.root
     }
 
@@ -62,6 +65,30 @@ class MyContactsFragment : Fragment() {
             AddContactDialogFragment().show(childFragmentManager, "AddContactDialog")
         }
     }
+
+    private fun swipeToDelete() {
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean { // There is no need to implement drag and drop, so we return false
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.bindingAdapterPosition
+                val deletedContact = viewHolder.itemView.tag as Contact
+                viewModel.deleteUser(deletedContact)
+//                contactsAdapter.showUndoSnackbar(deletedContact, position)  //todo move to viewModel
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(binding.recyclerViewContacts)
+    }
+}
 //
 //    private fun setupRecyclerView() {
 //        recyclerViewContacts = binding.recyclerViewContacts
@@ -179,4 +206,3 @@ class MyContactsFragment : Fragment() {
 ////        contactsAdapter.contacts.add(contact)
 ////        contactsAdapter.notifyItemInserted(contactsAdapter.contacts.size - 1)
 ////    }
-}
