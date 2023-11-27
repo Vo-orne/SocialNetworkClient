4b9d767b589d7388bdaabc107efaa12c81dc1db5
Vo-orne <tolya.rudyak2012@gmail.com>
2023-11-28 01:04:29 +0200

    SNC-5: change from 28.11.23 - A new layout for adding contacts was added;

---
+++ app/src/main/java/com/example/myprofile/presentation/ui/fragments/add_contact/AddContactFragment.kt
@@ -0,0 +1,11 @@
+package com.example.myprofile.presentation.ui.fragments.add_contact
+
+import com.example.myprofile.databinding.FragmentAddContactBinding
+import com.example.myprofile.presentation.ui.base.BaseFragment
+
+class AddContactFragment : BaseFragment<FragmentAddContactBinding>(FragmentAddContactBinding::inflate) {
+
+    override fun setListeners() {
+        TODO("Not yet implemented")
+    }
+}
