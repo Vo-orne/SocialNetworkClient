4830d10b2fea1d7e2c71867fd83e3e2ae2972b95
Vo-orne <tolya.rudyak2012@gmail.com>
2024-05-21 17:45:40 +0300

    SNC-6: change from 21.05.24 - Added push notification search button and switch to MainActivity when clicking on it or notification.

--- app/src/main/java/com/example/myprofile/di/NotificationModule.kt
+++ app/src/main/java/com/example/myprofile/di/NotificationModule.kt
@@ -2,12 +2,15 @@ package com.example.myprofile.di
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat.getString
 import com.example.myprofile.R
+import com.example.myprofile.presentation.ui.activity.MainActivity
 import com.example.myprofile.presentation.utils.Constants
 import dagger.Module
 import dagger.Provides
@@ -25,11 +28,36 @@ class NotificationModule {
     fun createNotificationBuilder(
         @ApplicationContext context: Context
     ): NotificationCompat.Builder {
+//        val ACTION_SNOOZE = "snooze"
+//
+//        val snoozeIntent = Intent(this, MyBroadcastReceiver::class.java).apply {
+//            action = ACTION_SNOOZE
+//            putExtra(EXTRA_NOTIFICATION_ID, 0)
+//        }
+//        val snoozePendingIntent: PendingIntent =
+//            PendingIntent.getBroadcast(this, 0, snoozeIntent, 0)
+//        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+//            .setSmallIcon(R.drawable.notification_icon)
+//            .setContentTitle("My notification")
+//            .setContentText("Hello World!")
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//            .setContentIntent(pendingIntent)
+//            .addAction(R.drawable.ic_snooze, getString(R.string.snooze),
+//                snoozePendingIntent)
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
         return NotificationCompat.Builder(context, Constants.CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(getString(context, R.string.app_name))
             .setContentText(getString(context, R.string.notification))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(0, getString(context, R.string.search), pendingIntent)
+            .setContentIntent(pendingIntent)
     }
 
     @Singleton
