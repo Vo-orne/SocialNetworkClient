8120618cf3703d76fe7815ee52a7443ea04e1ec8
Vo-orne <tolya.rudyak2012@gmail.com>
2024-08-17 15:55:17 +0300

    SNC-6: change from 14.08.24 - Finished the work of rooms.

--- app/src/main/java/com/example/myprofile/data/repository/ContactsRepository.kt
+++ app/src/main/java/com/example/myprofile/data/repository/ContactsRepository.kt
@@ -18,97 +18,97 @@ class ContactsRepository @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
 
-    /**
-     * List of contacts to be displayed.
-     */
-    private var contacts = mutableListOf<Contact>()
+//    /**
+//     * List of contacts to be displayed.
+//     */
+//    private var contacts = mutableListOf<Contact>()
 
     /**
      * List of listeners.
      */
     private val listeners = mutableListOf<UsersListener>()
 
-    /**
-     * A list of the last saved contacts that will be possible to return.
-     */
-    private var _lastDeletedContacts = mutableListOf<Contact>()
-    var lastDeletedContacts = _lastDeletedContacts
+//    /**
+//     * A list of the last saved contacts that will be possible to return.
+//     */
+//    private var _lastDeletedContacts = mutableListOf<Contact>()
+//    var lastDeletedContacts = _lastDeletedContacts
+//
+//
+//    /**
+//     * Deletes a contact from the contacts list at a specific index. It stores the deleted contact
+//     * in lastDeletedContact and its position in positionLastDeletedContact.
+//     * Then, it notifies listeners about the changes.
+//     * @param contact The contact to be deleted.
+//     * @param position The position of the contact to be deleted in the list.
+//     */
+//    fun deleteContact(contact: Contact, position: Int) {
+//        _lastDeletedContacts.clear()
+//        deleteItem(contact, position)
+//    }
+//
+//    /**
+//     * Removes a specific contact from the contact list.
+//     * @param contact Contact to be deleted.
+//     * @param position The position of the contact in the contact list.
+//     */
+//    private fun deleteItem(contact: Contact, position: Int) {
+//        val indexToDelete = contacts.indexOfFirst { it.id == contact.id }
+//        if (indexToDelete != -1) {
+//            contacts = ArrayList(contacts)
+//            contacts.removeAt(position)
+//            _lastDeletedContacts.add(contact)
+//            notifyChanges()
+//        }
+//    }
 
-
-    /**
-     * Deletes a contact from the contacts list at a specific index. It stores the deleted contact
-     * in lastDeletedContact and its position in positionLastDeletedContact.
-     * Then, it notifies listeners about the changes.
-     * @param contact The contact to be deleted.
-     * @param position The position of the contact to be deleted in the list.
-     */
-    fun deleteContact(contact: Contact, position: Int) {
-        _lastDeletedContacts.clear()
-        deleteItem(contact, position)
-    }
-
-    /**
-     * Removes a specific contact from the contact list.
-     * @param contact Contact to be deleted.
-     * @param position The position of the contact in the contact list.
-     */
-    private fun deleteItem(contact: Contact, position: Int) {
-        val indexToDelete = contacts.indexOfFirst { it.id == contact.id }
-        if (indexToDelete != -1) {
-            contacts = ArrayList(contacts)
-            contacts.removeAt(position)
-            _lastDeletedContacts.add(contact)
-            notifyChanges()
-        }
-    }
-
-    /**
-     * Deletes all selected contacts in multiselect mode.
-     * @param selectedContacts List of selected contacts transferred View model.
-     */
-    fun deleteSelectedContacts(selectedContacts: HashSet<Pair<Contact, Int>>) {
-        // Clears previously deleted contacts so that _lastDeletedContacts contains only
-        // the last deleted contacts.
-        _lastDeletedContacts.clear()
-        val deletedContacts = mutableListOf<Contact>()
-
-        // Sort selected Contacts in descending index order,
-        // to delete contacts with the current index
-        selectedContacts.sortedByDescending { it.second }
-        for (contact in selectedContacts) {
-            val indexToDelete = contacts.indexOfFirst { it.id == contact.first.id }
-            if (indexToDelete != -1) {
-                contacts = ArrayList(contacts)
-                contacts.removeAt(indexToDelete)
-                deletedContacts.add(contact.first)
-            }
-        }
-        _lastDeletedContacts.addAll(deletedContacts)
-        notifyChanges()
-    }
-
-    fun clearLastDeletedContact() {
-        _lastDeletedContacts.clear()
-    }
-
-    /**
-     * Adds a new contact to the contacts list and notifies listeners about the changes.
-     * @param contact The contact to be added.
-     */
-    fun addContact(contact: Contact) {
-        contacts = ArrayList(contacts)
-        contacts.add(contacts.size, contact)
-        notifyChanges()
-    }
-
-    /**
-     * Adds a listener to the listeners list and notifies it about the current contacts list.
-     * @param listener The listener to be added.
-     */
-    fun addListener(listener: UsersListener) {
-        listeners.add(listener)
-        listener.invoke(contacts)
-    }
+//    /**
+//     * Deletes all selected contacts in multiselect mode.
+//     * @param selectedContacts List of selected contacts transferred View model.
+//     */
+//    fun deleteSelectedContacts(selectedContacts: HashSet<Pair<Contact, Int>>) {
+//        // Clears previously deleted contacts so that _lastDeletedContacts contains only
+//        // the last deleted contacts.
+//        _lastDeletedContacts.clear()
+//        val deletedContacts = mutableListOf<Contact>()
+//
+//        // Sort selected Contacts in descending index order,
+//        // to delete contacts with the current index
+//        selectedContacts.sortedByDescending { it.second }
+//        for (contact in selectedContacts) {
+//            val indexToDelete = contacts.indexOfFirst { it.id == contact.first.id }
+//            if (indexToDelete != -1) {
+//                contacts = ArrayList(contacts)
+//                contacts.removeAt(indexToDelete)
+//                deletedContacts.add(contact.first)
+//            }
+//        }
+//        _lastDeletedContacts.addAll(deletedContacts)
+//        notifyChanges()
+//    }
+//
+//    fun clearLastDeletedContact() {
+//        _lastDeletedContacts.clear()
+//    }
+//
+//    /**
+//     * Adds a new contact to the contacts list and notifies listeners about the changes.
+//     * @param contact The contact to be added.
+//     */
+//    fun addContact(contact: Contact) {
+//        contacts = ArrayList(contacts)
+//        contacts.add(contacts.size, contact)
+//        notifyChanges()
+//    }
+//
+//    /**
+//     * Adds a listener to the listeners list and notifies it about the current contacts list.
+//     * @param listener The listener to be added.
+//     */
+//    fun addListener(listener: UsersListener) {
+//        listeners.add(listener)
+//        listener.invoke(contacts)
+//    }
 
     /**
      * Removes a listener from the listeners list.
@@ -117,11 +117,11 @@ class ContactsRepository @Inject constructor(
     fun removeListener(listener: UsersListener) {
         listeners.remove(listener)
     }
-
-    /**
-     * Notifies all listeners about the changes in the contacts list.
-     */
-    private fun notifyChanges() {
-        listeners.forEach { it.invoke(contacts) }
-    }
+//
+//    /**
+//     * Notifies all listeners about the changes in the contacts list.
+//     */
+//    private fun notifyChanges() {
+//        listeners.forEach { it.invoke(contacts) }
+//    }
 }
