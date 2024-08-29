5c8ff9363f6896712f99a6c507c93dd0107e4f8e
Vo--rne <tolya.rudyak2012@gmail.com>
2024-08-29 16:58:30 +0300

    29.08.2024 - Removed all references to the repository that was before Room and the Retrofit2;;

--- app/src/main/java/com/example/myprofile/di/RepositoryModule.kt
+++ app/src/main/java/com/example/myprofile/di/RepositoryModule.kt
@@ -1,7 +1,5 @@
 package com.example.myprofile.di
 
-import android.content.Context
-import com.example.myprofile.data.repository.ContactsRepository
 import com.example.myprofile.data.repository.UserRepositoryImpl
 import com.example.myprofile.data.repository.UsersRepositoryImpl
 import com.example.myprofile.domain.ApiService
@@ -9,7 +7,6 @@ import com.example.myprofile.domain.UsersApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import javax.inject.Singleton
@@ -17,13 +14,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
-
-    @Provides
-    fun provideContactRepository(
-        @ApplicationContext context: Context
-    ): ContactsRepository {
-        return ContactsRepository(context)
-    }
 
     @Provides
     fun providesRepositoryImpl(
